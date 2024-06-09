4562c0351367355697a99b85aa7c32a767f0a498
Christine Anthony <2christineanthony@gmail.com>
2024-06-09 18:43:08 -0400

    junk

--- test_US02_US29.py
+++ test_US02_US29.py
@@ -1,8 +1,10 @@
 import Pro3_2
 
 
-# def test_answer():
-#     assert Pro3_2.func(3) == 5
+def test_answer():
+    assert Pro3_2.func(3) == 5#, "should have been 4"
 
 def test_check_birth_before_marriage(indi):
-  assert Pro3_2.check_birth_before_marriage(indi) == True
+  #assert Pro3_2.check_birth_before_marriage(indi) == True
+  #for each indi, get DOB, get DOmarriage, asssert below
+  assert DOB (current_indi["Birthday"]) < DOmarriage
