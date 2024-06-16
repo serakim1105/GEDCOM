5c9bfef7426a7da9ba526a708eb3f7e690901855
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2024-06-15 20:23:43 -0400

    Update us30.py

--- us30.py
+++ us30.py
@@ -1,13 +1,16 @@
-def us30(individuals,families):
-    # living_individuals = []
-    # living_married_individuals = []
+## List all living married individuals
+def us30(individuals, families):
+    living_individuals = []
+    living_married_individuals = []
     errors = []
     print("\nAll living married individuals:")
-    for fam in families:
 
-        husband_id = fam["HusbandName"]
-        for husband_id in fam["HusbandName"]:
-            for indi in individuals:
-                if indi["Name"] == husband_id:
-                    errors.append(f'ERROR: INDIVIDUAL: US30: {indi["ID"]}: Not living and married.')
-    return errors
+    for fam in families:
+        married = fam["Married"] != "NA"
+        for indi in individuals:
+            alive = indi["Death"] != "NA" 
+            if alive and married:
+                living_married_individuals.append(indi["Name"])
+            else:
+                errors.append(f'ERROR: INDIVIDUAL: US30: {indi["Name"]}: Not living and married.')
+        return errors
