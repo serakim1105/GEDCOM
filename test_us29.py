814caa8b1333ba486c58b24645d0275860aa22a0
Christine Anthony <2christineanthony@gmail.com>
2024-06-17 20:33:46 -0400

    udpate us29 test

--- test_us29.py
+++ test_us29.py
@@ -10,9 +10,9 @@ def create_indi(id, name, death_date="NA"):
         "Death": death_date,
     }
 indi01 = create_indi("I01", "Christine /Anthony/", "20 OCT 1942") #deceased
-indi02 = create_indi("I01", "Sriya /Bhamidipati/", "01 NOV 2024") #deceased
-indi03 = create_indi("I02", "Sera /Kim/")                         #not deceased
-indi03 = create_indi("I02", "Navya /Pai/", "NA")                  #not deceased
+indi02 = create_indi("I02", "Sriya /Bhamidipati/", "01 NOV 2024") #deceased
+indi03 = create_indi("I03", "Sera /Kim/")                         #not deceased
+indi03 = create_indi("I04", "Navya /Pai/", "NA")                  #not deceased
 
 individuals = []
 individuals.append(indi01)
@@ -20,7 +20,7 @@ individuals.append(indi02)
 individuals.append(indi03)
 
 def test_1_us29(): 
-    assert us29(individuals) == ["Christine /Anthony/", "Sriya /Bhamidipati/"] #should pass
+    assert us29(individuals) == ["Individual: I01: Christine Anthony\n", "Individual: I02: Sriya Bhamidipati\n"] #should pass
 
 def test_2_us29():
     assert len(us29(individuals)) == 2
