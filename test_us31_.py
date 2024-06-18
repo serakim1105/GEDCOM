a8b0cb1a075ed3825de2f7d1e81d2706e2d07302
NavyaPai <painayav@gmail.com>
2024-06-17 19:32:57 -0500

    sriyas test file

--- test_us31_.py
+++ test_us31_.py
@@ -18,29 +18,29 @@ def create_individual(id, birth_date = "NA", death_date = "NA", spouse = ["NA"])
 def test_1():
     # Test case 1: Living Individual who is single and above 30
     indi1 = create_individual("I01", "01 JAN 1950", "NA", ["F01"])
-    assert us31([indi1]) == [f'ERROR: INDIVIDUAL: US31: {indi1["ID"]}: Is not alive or married above 30.']
+    assert us31([indi1]) == [f'ERROR: INDIVIDUAL: US31: {indi1["ID"]}: Is not alive or single above 30.']
 
 def test_2():
     # Test case 2: Living Individual who is single and above 30
     indi2 = create_individual("I04", "12 OCT 2000", "NA", ["NA"])
-    assert us31([indi2]) == [f'ERROR: INDIVIDUAL: US31: {indi2["ID"]}: Is not alive or married above 30.']
+    assert us31([indi2]) == [f'ERROR: INDIVIDUAL: US31: {indi2["ID"]}: Is not alive or single above 30.']
 
 def test_3():
     # Test case 2: Living Individual who is single and above 30
     indi3 = create_individual("I05", "5 NOV 1980", "15 OCT 1998", ["NA"])
-    assert us31([indi3]) == [f'ERROR: INDIVIDUAL: US31: {indi3["ID"]}: Is not alive or married above 30.']
+    assert us31([indi3]) == [f'ERROR: INDIVIDUAL: US31: {indi3["ID"]}: Is not alive or single above 30.']
 
 def test_4():
     # Test case 2: Living Individual who is single and above 30
     birthToday = datetime.today().strftime("%d %b %Y")
     indi4 = create_individual("I05", birthToday, "NA", ["NA"])
-    assert us31([indi4]) == [f'ERROR: INDIVIDUAL: US31: {indi4["ID"]}: Is not alive or married above 30.']
+    assert us31([indi4]) == [f'ERROR: INDIVIDUAL: US31: {indi4["ID"]}: Is not alive or single above 30.']
 
 def test_5():
     # Test case 2: Living Individual who is single and above 30
     birthToday = datetime.today().strftime("%d %b %Y")
     indi5 = create_individual("I08", birthToday, "NA", ["F02"])
-    assert us31([indi5]) == [f'ERROR: INDIVIDUAL: US31: {indi5["ID"]}: Is not alive or married above 30.']
+    assert us31([indi5]) == [f'ERROR: INDIVIDUAL: US31: {indi5["ID"]}: Is not alive or single above 30.']
 
 
 if __name__ == "__main__":
