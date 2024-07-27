a247406a202d7a641626e74a3d7c1368dcbd1931
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2024-07-27 02:52:24 -0400

    Update ProjectAgile.py

--- ProjectAgile.py
+++ ProjectAgile.py
@@ -497,6 +497,55 @@ def us16(individuals, families):
                     if husband_last_name and child_last_name != husband_last_name:
                         errors.append(f'Line {indi["line"]} - Family {fam["ID"]}: Male child ({child_last_name}) has a different last name than the father ({husband_last_name})')
     return errors
+
+#US17: Parents should not marry any of their descendants
+def us17(families):
+    errors =[]
+    parents = {}
+
+    for fam in families:
+        husband_id = fam.get('Husband')
+        wife_id = fam.get('Wife')
+        children_ids = fam.get('Children', [])
+
+        if husband_id:
+            if husband_id not in parents:
+                parents[husband_id] = set()
+            parents[husband_id].update(children_ids)
+        
+        if wife_id:
+            if wife_id not in parents:
+                parents[wife_id] = set()
+            parents[wife_id].update(children_ids)
+    
+    for fam in families:
+        husband_id = fam.get('Husband')
+        wife_id = fam.get('Wife')
+
+        if husband_id in parents and wife_id in parents[husband_id]:
+            errors.append(f"Error US17: Parent {husband_id} should not marry descendant {wife_id} in family {fam['ID']}.")
+        
+        if wife_id in parents and husband_id in parents[wife_id]:
+            errors.append(f"Error US17: Parent {wife_id} should not marry descendant {husband_id} in family {fam['ID']}.")
+
+    return errors
+
+#US18: Siblings should not marry one another
+def us18(families):
+    errors =[]
+    AllChildrens = {}
+    
+    for fam in families:
+        if 'Children' in fam and fam['Children']:
+            for child in fam['Children']:
+                AllChildrens[child] = fam['Children']
+
+    for fam in families:
+        husband = fam.get('Husband')
+        wife = fam.get('Wife')
+        if husband in AllChildrens and wife in AllChildrens and AllChildrens[husband] == AllChildrens[wife]:
+            errors.append(f"Error US18: Siblings {husband} and {wife} should not marry one another in family {fam['ID']}.")
+    return errors
     
 # US22: All individual IDs should be unique and all family IDs should be unique
 def us22(individuals, families):
@@ -1049,6 +1098,14 @@ def main():
     # Check for US16: Male last names
     errors_us16 = us16(individuals, families)
     print_errors(errors_us16, 'US16')
+
+    # Check for US17: No marriages to descendants
+    errors_us17 = us17(families)
+    print_errors(errors_us17, 'US17', 'No marriages to descendants')
+
+    # Check for US18: Siblings should not marry
+    errors_us18 = us18(families)
+    print_errors(errors_us18, 'US18', 'Siblings should not marry')
 
     # Check for US22: Unique IDs
     errors_us22 = us22(individuals, families)
