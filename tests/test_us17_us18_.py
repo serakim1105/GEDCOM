581bc3c2f62119aca5d066088a64ebd0cbe57757
NavyaPai <painayav@gmail.com>
2024-07-27 00:57:55 -0500

    adding test case to us17

--- tests/test_us17_us18_.py
+++ tests/test_us17_us18_.py
@@ -20,6 +20,41 @@ def create_family(id, husband_id, wife_id, children_ids):
         "Wife": wife_id,
         "Children": children_ids
     }
+def test_1_us17():
+    # Test case 1: Parent married to a child
+    
+    family1 = create_family("F1", "I1", "I2", ["I3"])
+    family2 = create_family("F2", "I1", "I3", [])
+
+    families = [family1, family2]
+
+    expected_output = ["Error US17: Parent I1 should not marry descendant I3 in family F2."]
+
+    assert us17(families) == expected_output
+
+def test_2_us17():
+    # Test case 2: No parent married to a child
+
+    family1 = create_family("F1", "I1", "I2", ["I3"])
+    family2 = create_family("F2", "I3", "I4", [])
+
+    families = [family1, family2]
+
+    expected_output = []
+
+    assert us17(families) == expected_output
+
+def test_3_us17():
+    # Test case 3: Parent married to a child 
+    family1 = create_family("F1", "I1", "I2", ["I3", "I4"])
+    family2 = create_family("F2", "I2", "I4", [])
+
+    
+    families = [family1, family2]
+
+    expected_output = ["Error US17: Parent I2 should not marry descendant I4 in family F2."]
+
+    assert us17(families) == expected_output
 
 def test_1_us18():
     # Test case 1: Siblings are married
